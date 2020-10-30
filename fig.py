7bd0a3d86d8c388d54b9c4127d046618556eba47
tks3141 <28818747+tks3141@users.noreply.github.com>
2020-10-30 20:41:41 +0900

    add posting flask and edit msg

---
+++ fig.py
@@ -0,0 +1,5 @@
+from pyfiglet import Figlet
+
+f = Figlet(font="big")
+msg = f.renderText("hello world")
+print(msg)
--- nfc_reader/bin/nfc_reader.py
+++ nfc_reader/bin/nfc_reader.py
@@ -10,17 +10,25 @@ import requests
 
 import datetime
 import sys
+from pyfiglet import Figlet
 
 # タッチされたあとの次の待受時間
 TIME_WAIT = 5
-API_URL = 'https://ancient-woodland-85036.herokuapp.com/raspi'
+SLACK_API_URL = 'https://ancient-woodland-85036.herokuapp.com/raspi'
+FLASK_API_URL = 'sum:url/record'
 log_path = '/var/log/nfc.log'
 
-def post_nfc(idm,ent_time):
+
+def render_big_msg(msg):
+    f = Figlet(font="big")
+    msg = f.renderText("hello world")
+    print(msg)
+    # return msg
+
+def post_nfc(url,idm,send_msg):
     assert type(idm) == str
     assert type(ent_time) == str
-
-    send_msg = ent_time + 'に'
+    
     print('post:',idm,send_msg)
     # print(idm,ent_time,file=open(log_path, 'a+'))
     # with open(log_path, mode='a') as f:
@@ -46,15 +54,18 @@ def main():
 
             if tag.TYPE == 'Type3Tag':
                 idm = tag.identifier.hex()
-                ent_time = (datetime.datetime.now().strftime('%m月%d日 %H:%M'))
-                # print(idm,ent_time)
-                # print()
-                post_nfc(idm,ent_time)
-                print('idm',idm,datetime.datetime.now(),file=open(log_path, 'a+'))
 
+                time_now = datetime.datetime.now()
+                send_msg = time_now.strftime('%m月%d日 %H:%M'))+'に'
+
+                print('idm',idm,time_now,file=open(log_path, 'a+'))
+                post_nfc(SLACK_API_URL,idm,send_msg)
+                post_nfc(FLASK_API_URL,idm,'')
+                
                 time.sleep(TIME_WAIT)
-                print('ready for read !!')
+                render_big_msg("READY FOR READING NFC ... ")
             else:
+                render_big_msg("OOPS!!!")
                 print(tag+'\n read but Only Type3Tag is supported now.')
 
     clf.close()
@@ -66,5 +77,4 @@ print('aaa')
 if __name__ == "__main__":
     sys.stdout = open('/dev/console', 'w')
     print('nfc_reader run',datetime.datetime.now(),file=open(log_path, 'a+'))
-    print('start nfc !!')
     main()
